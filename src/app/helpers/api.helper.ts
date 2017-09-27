bc6ad211f47811ee9337f0cc0502cf0e1e8f0a7e
hardeep.singh <hardeep.singh@mindfulsas.com>
2017-09-27 18:31:07 +0530

    api fixes

--- src/app/helpers/api.helper.ts
+++ src/app/helpers/api.helper.ts
@@ -1,10 +1,19 @@
 import { Observable } from 'rxjs/Rx';
+// import { ResponseHelper } from './response.helper';
+import { Data ,Page,Success,Failure} from "app/models/responses.model";
 
-export interface ApiInterface<DataModel> {
-    get(id: string):Promise<DataModel>,
-    put(id: string, model: any): Promise<DataModel>,
-    post(model: any): Promise<DataModel>,
+export interface ApiInterface<DataModel>
+    extends Data<DataModel>, Page<DataModel>, Success, Failure
+ {
+    
+    get(id: string): Promise<Data<DataModel>>,
 
-    delete(id: string), //TODO
-    search() //TODO
+    put(id: string, model: DataModel): Promise<Data<DataModel>>,
+
+    post(model: DataModel): Promise<Data<DataModel> | Failure>,
+
+    delete(id: string): Promise<any>,
+
+    search(query: object): Promise<Page<DataModel>>
+    
 }
